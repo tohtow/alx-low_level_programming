dc56fc4e78d3cb31b551b4765567feb1fb4947b2
tohtow <hellenjacob24@gmail.com>
2022-10-16 12:03:21 -0700

    Hellen best

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always o
+ */
+int main(void)
+ {
+        puuts("\"programming is like building a multilingual puzzle");
+        return (o);
+  }	
